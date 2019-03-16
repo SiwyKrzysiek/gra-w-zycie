c230122138da66b7e150e8c5e3c06bb65e407c4c
SiwyKrzysiek <k.fajny@gmail.com>
2019-03-16 22:39:57 +0100

    Test jednostkowy boardToString()

---
+++ Program/boardTest.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdlib.h>
+#include <string.h>
+#include <CUnit/CUnit.h>
+#include "board.h"
+
+void testBoardToString();
--- Program/main.c
+++ Program/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <CUnit/Basic.h>
 
 #include "board.h"
 #include "parser.h"
+#include "boardTest.h"
+
+#define TESTS
+
+#ifdef TESTS
+void runTests();
+#endif
 
 int main()
 {
@@ -13,16 +21,51 @@ int main()
     // puts(result);
 
     // free(result);
+    #ifdef TESTS
+        runTests();
+    #endif
+    
 
-    Board board = loadBoardFromFile("input.txt");
+    // Board board = loadBoardFromFile("input.txt");
 
-    puts("--------------------------------------\n");
-    char *serialized =  serializeBoard(&board);
+    // puts("--------------------------------------\n");
+    // char *serialized =  serializeBoard(&board);
 
-    printf("%s\n", serialized);
-    free(serialized);
+    // printf("%s\n", serialized);
+    // free(serialized);
 
-    disposeBoard(&board);
+    // disposeBoard(&board);
 
     return EXIT_SUCCESS;
 }
+
+#ifdef TESTS
+void runTests()
+{
+    CU_pSuite boardSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if (CUE_SUCCESS != CU_initialize_registry())
+      exit(CU_get_error());
+
+   /* add a suite to the registry */
+   boardSuite = CU_add_suite("Board tests", NULL, NULL);
+   if (NULL == boardSuite) {
+      CU_cleanup_registry();
+      exit(CU_get_error());
+   }
+
+   /* add the tests to the suite */
+   if (NULL == CU_add_test(boardSuite, "To string test", testBoardToString))
+   {
+      CU_cleanup_registry();
+      exit(CU_get_error());
+   }
+
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   CU_cleanup_registry();
+
+   exit(CU_get_error());
+}
+#endif
